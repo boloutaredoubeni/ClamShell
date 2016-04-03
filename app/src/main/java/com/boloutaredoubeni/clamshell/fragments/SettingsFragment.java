30b54d38f87bcfa24515b09b21ac929ba5bd336d
Boloutare Doubeni <tarecd@gmail.com>
2016-04-03 13:38:56 -0400

    Add butterknife binding

--- app/src/main/java/com/boloutaredoubeni/clamshell/fragments/SettingsFragment.java
+++ app/src/main/java/com/boloutaredoubeni/clamshell/fragments/SettingsFragment.java
@@ -17,6 +17,6 @@ public class SettingsFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_app_list, container, false);
+    return inflater.inflate(R.layout.fragment_dummy, container, false);
   }
 }
