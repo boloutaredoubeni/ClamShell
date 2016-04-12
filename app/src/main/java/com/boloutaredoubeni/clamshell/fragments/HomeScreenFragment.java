a298d02685670b126c9c804c31a17bdcf3213722
Boloutare Doubeni <tarecd@gmail.com>
2016-04-12 10:16:52 -0400

    Refactoring layout

--- app/src/main/java/com/boloutaredoubeni/clamshell/fragments/HomeScreenFragment.java
+++ app/src/main/java/com/boloutaredoubeni/clamshell/fragments/HomeScreenFragment.java
@@ -27,7 +27,7 @@ public class HomeScreenFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_dummy, container, false);
+    View view = inflater.inflate(R.layout.fragment_home_screen, container, false);
     ButterKnife.bind(this, view);
     return view;
   }
